ee408a61c02615757aed5e57c6a6c2773ab51cd6
inicius <viniciusf84@hotmail.com>
2020-09-03 01:04:10 -0300

    [ + ] Create general styles

---
+++ src/styles/global.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  html, body, #root {
+    height: 100%;
+    margin: 0;
+  }
+
+  body {
+    background: #fff;
+    -webkit-font-smoothing: antialiased;
+    font: 16px Montserrat, sans-serif;
+  }
+
+  #root {
+    > section {    
+      height: 100%;       
+      padding: 20px;
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+    }
+  }
+`;
---
+++ src/styles/variables.ts
@@ -0,0 +1,6 @@
+export const colors = {
+	primary: '#2C97D1',
+	clear: '#FFFFFF',
+	dark: '#2A2A2A',
+	light: '#FBFE63',
+};
