8b2d31c74f758d2799cb1600be369912d387d3b2
inicius <viniciusf84@hotmail.com>
2020-09-03 00:58:45 -0300

    [ * ] Add routes to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-export default function App() {
-	return (
-		<>
-			<p>Hello World!</p>
-		</>
-	);
-}
+import { MasterProvider } from './contexts/MasterContext';
+
+import GlobalStyle from './styles/global';
+
+import Routes from './routes';
+
+const App: React.FC = () => (
+	<MasterProvider>
+		<BrowserRouter>
+			<Routes />
+		</BrowserRouter>
+		<GlobalStyle />
+	</MasterProvider>
+);
+
+export default App;
