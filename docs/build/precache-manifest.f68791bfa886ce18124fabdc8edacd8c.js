33eaa12c7dadc1eb8a454b2236d0efc92b5b974e
inicius <viniciusf84@hotmail.com>
2020-09-05 18:30:49 -0300

    iClinic go live!

---
+++ docs/build/precache-manifest.f68791bfa886ce18124fabdc8edacd8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e992791ce09cf896d6925433ac9c6cd8",
+    "url": "/challenge-iclinic/index.html"
+  },
+  {
+    "revision": "e2e7200e3ca8796c0418",
+    "url": "/challenge-iclinic/static/js/2.607a6a03.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/challenge-iclinic/static/js/2.607a6a03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e64a8b80ca2812889c1d",
+    "url": "/challenge-iclinic/static/js/main.8823a806.chunk.js"
+  },
+  {
+    "revision": "9bf235b99fd119594c0a",
+    "url": "/challenge-iclinic/static/js/runtime-main.8ae1d8cb.js"
+  },
+  {
+    "revision": "3e06b3a8400a31f968fb37d4a368c9fe",
+    "url": "/challenge-iclinic/static/media/darth-vader.3e06b3a8.png"
+  },
+  {
+    "revision": "738fc4804a1b605797968b2f1446769f",
+    "url": "/challenge-iclinic/static/media/luke-skywalker.738fc480.png"
+  }
+]);
