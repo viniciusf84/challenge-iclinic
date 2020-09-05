e054d5ea70d988424670a7a6b3c07b2e8dcf9f01
inicius <viniciusf84@hotmail.com>
2020-09-05 18:52:32 -0300

    [ * ] Go live!

---
+++ docs/build/precache-manifest.b90a2c2a7f3d6a39895929b655f85ade.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a886412b95ba09bbe686486e1593cfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98164cca255b43731918",
+    "url": "/static/js/2.b6f25117.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b6f25117.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e64a8b80ca2812889c1d",
+    "url": "/static/js/main.8823a806.chunk.js"
+  },
+  {
+    "revision": "6801fb219dc7bde16d35",
+    "url": "/static/js/runtime-main.c095378d.js"
+  },
+  {
+    "revision": "3e06b3a8400a31f968fb37d4a368c9fe",
+    "url": "/static/media/darth-vader.3e06b3a8.png"
+  },
+  {
+    "revision": "738fc4804a1b605797968b2f1446769f",
+    "url": "/static/media/luke-skywalker.738fc480.png"
+  }
+]);
