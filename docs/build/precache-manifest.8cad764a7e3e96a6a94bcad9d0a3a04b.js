8215de958777b2c388d6189315c0ef099fd8b54f
inicius <viniciusf84@hotmail.com>
2020-09-06 01:02:02 -0300

    [ * ] mv build docs

---
+++ docs/build/precache-manifest.8cad764a7e3e96a6a94bcad9d0a3a04b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bfe999f61cd1df8440e00ad360d53c1",
+    "url": "/iclinic/index.html"
+  },
+  {
+    "revision": "4c4edf3fc7b1563e7f0f",
+    "url": "/iclinic/static/js/2.29dd1e7c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/iclinic/static/js/2.29dd1e7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ed2acb352bd8e0ea38f",
+    "url": "/iclinic/static/js/main.0b6aaa79.chunk.js"
+  },
+  {
+    "revision": "9a7033bb3fe16d0acbc2",
+    "url": "/iclinic/static/js/runtime-main.897abc14.js"
+  },
+  {
+    "revision": "3e06b3a8400a31f968fb37d4a368c9fe",
+    "url": "/iclinic/static/media/darth-vader.3e06b3a8.png"
+  },
+  {
+    "revision": "738fc4804a1b605797968b2f1446769f",
+    "url": "/iclinic/static/media/luke-skywalker.738fc480.png"
+  }
+]);
